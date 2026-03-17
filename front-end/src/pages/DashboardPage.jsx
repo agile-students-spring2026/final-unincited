39d048badc19f0812fd5a36804b66121a1dc177b
n3xta <n333xta@gmail.com>
2026-03-17 10:55:53 -0400

    Added mock data. Updated Dashboard with an example usage of mock data.

--- front-end/src/pages/DashboardPage.jsx
+++ front-end/src/pages/DashboardPage.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import './DashboardPage.css'
 import ArticleCard from "../components/ArticleCard";
+import { mockArticles } from "../data/mockData";
 
-//need to inject mock data for article list
 function DashboardPage() {
 
   const [searchTerm, setSearchTerm] = useState("");
@@ -14,7 +14,17 @@ function DashboardPage() {
       <div className="dashboard-title">Recent Articles</div>
       <div className="dashboard-subtitle">Articles with bias analysis</div>
       <div className="articles-list">
-          
+          {mockArticles.map((article) => (
+            <ArticleCard
+              key={article.id}
+              source={article.sourceName}
+              title={article.title}
+              summary={article.summary}
+              date={article.publishDate}
+              sentiment={article.analysis.sentiment.label}
+              bias={article.analysis.bias.label}
+            />
+          ))}
       </div>
 
   </div>
