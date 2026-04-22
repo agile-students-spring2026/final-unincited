6f0121d79157f9e9b090f0612596ccdb072c631c
danapantoja <danapantoja145@gmail.com>
2026-04-21 20:46:30 -0400

    mock user

---
+++ back-end/mockUsers.js
@@ -0,0 +1,12 @@
+const mockUsers = [
+    {
+      id: 1,
+      name: 'Test User',
+      email: 'test@example.com',
+      password: 'password123',
+      savedArticles: [],
+      submittedArticles: [],
+    },
+  ]
+  
+  export default mockUsers
